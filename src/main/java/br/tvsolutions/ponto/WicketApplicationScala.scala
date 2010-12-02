e707be5946e775871a316ff9fb72bbaf9959168b
Tássio Virgínio <tassiovirginio@gmail.com>
2010-12-02 05:19:14 -0300

    Configurado para funcionar com o HSQLDB em memoria, com usuario:admin e senha: admin. Criado o README.

--- src/main/java/br/tvsolutions/ponto/WicketApplicationScala.scala
+++ src/main/java/br/tvsolutions/ponto/WicketApplicationScala.scala
@@ -3,6 +3,7 @@ package br.tvsolutions.ponto
 import org.apache.wicket.protocol.http.WebApplication
 import br.tvsolutions.ponto.mediators.TPontoMediatorScala
 import br.tvsolutions.ponto.mediators.TUsuarioMediatorScala
+import br.tvsolutions.ponto.entities.Usuario
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector
@@ -23,7 +24,20 @@ class WicketApplicationScala extends WebApplication{
 	override def init() = {
 		addComponentInstantiationListener(new SpringComponentInjector(this))
 		getDebugSettings().setAjaxDebugModeEnabled(false)
+		
+		//Cadastro do Administrador do Sistema para Testes/Desenvolvimento
+		var usuario = new Usuario
+		usuario.setAdm(true)
+		usuario.setEmail("admin@email.com")
+		usuario.setLogin("admin")
+		usuario.setSenha("admin")
+		usuario.setNome("Admin")
+		usuario.setIps("127.0.0.1")
+		usuario.setWallpaper("wallpaper02")
+		usuarioMediatorScala.salvarUsuario(usuario)
+		
 		getApplicationSettings().setPageExpiredErrorPage(classOf[LoginPageScala])
+		
 	}
 
 }
