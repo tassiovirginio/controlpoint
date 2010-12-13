26cf96281c3e55e6dfb6fad4f8908477629a9f75
Tássio Virgínio <tassiovirginio@gmail.com>
2010-12-12 21:09:07 -0300

    Colocando as classes em java para scala.

--- src/main/java/br/tvsolutions/ponto/util/GerenciadorHSQLDB.java
+++ src/main/java/br/tvsolutions/ponto/util/GerenciadorHSQLDB.java
@@ -1,15 +1,15 @@
-package br.tvsolutions.ponto.util;
-
-import org.hsqldb.util.DatabaseManager;
-
-public class GerenciadorHSQLDB {
-
-	
-	public static void main(String[] args) {
-//		java -classpath ./lib/hsqldb.jar org.hsqldb.util.DatabaseManager
-		DatabaseManager databaseManager = new DatabaseManager();
-		databaseManager.start();
-
-	}
-
-}
+//package br.tvsolutions.ponto.util;
+//
+//import org.hsqldb.util.DatabaseManager;
+//
+//public class GerenciadorHSQLDB {
+//
+//	
+//	public static void main(String[] args) {
+////		java -classpath ./lib/hsqldb.jar org.hsqldb.util.DatabaseManager
+//		DatabaseManager databaseManager = new DatabaseManager();
+//		databaseManager.start();
+//
+//	}
+//
+//}
--- src/main/java/br/tvsolutions/ponto/util/PontoXLS.java
+++ src/main/java/br/tvsolutions/ponto/util/PontoXLS.java
@@ -1,52 +1,52 @@
-package br.tvsolutions.ponto.util;
-import java.util.Date;
-
-
-public class PontoXLS {
-	private String dia;
-	private Date horaEntrada1;
-	private Date horaSaida1;
-	private Date horaEntrada2;
-	private Date horaSaida2;
-	private String horaExtra;
-	
-	
-	public String getDia() {
-		return dia;
-	}
-	public void setDia(String dia) {
-		this.dia = dia;
-	}
-	public Date getHoraEntrada1() {
-		return horaEntrada1;
-	}
-	public void setHoraEntrada1(Date horaEntrada1) {
-		this.horaEntrada1 = horaEntrada1;
-	}
-	public Date getHoraSaida1() {
-		return horaSaida1;
-	}
-	public void setHoraSaida1(Date horaSaida1) {
-		this.horaSaida1 = horaSaida1;
-	}
-	public Date getHoraEntrada2() {
-		return horaEntrada2;
-	}
-	public void setHoraEntrada2(Date horaEntrada2) {
-		this.horaEntrada2 = horaEntrada2;
-	}
-	public Date getHoraSaida2() {
-		return horaSaida2;
-	}
-	public void setHoraSaida2(Date horaSaida2) {
-		this.horaSaida2 = horaSaida2;
-	}
-	public String getHoraExtra() {
-		return horaExtra;
-	}
-	public void setHoraExtra(String horaExtra) {
-		this.horaExtra = horaExtra;
-	}
-	
-
-}
+//package br.tvsolutions.ponto.util;
+//import java.util.Date;
+//
+//
+//public class PontoXLS {
+//	private String dia;
+//	private Date horaEntrada1;
+//	private Date horaSaida1;
+//	private Date horaEntrada2;
+//	private Date horaSaida2;
+//	private String horaExtra;
+//	
+//	
+//	public String getDia() {
+//		return dia;
+//	}
+//	public void setDia(String dia) {
+//		this.dia = dia;
+//	}
+//	public Date getHoraEntrada1() {
+//		return horaEntrada1;
+//	}
+//	public void setHoraEntrada1(Date horaEntrada1) {
+//		this.horaEntrada1 = horaEntrada1;
+//	}
+//	public Date getHoraSaida1() {
+//		return horaSaida1;
+//	}
+//	public void setHoraSaida1(Date horaSaida1) {
+//		this.horaSaida1 = horaSaida1;
+//	}
+//	public Date getHoraEntrada2() {
+//		return horaEntrada2;
+//	}
+//	public void setHoraEntrada2(Date horaEntrada2) {
+//		this.horaEntrada2 = horaEntrada2;
+//	}
+//	public Date getHoraSaida2() {
+//		return horaSaida2;
+//	}
+//	public void setHoraSaida2(Date horaSaida2) {
+//		this.horaSaida2 = horaSaida2;
+//	}
+//	public String getHoraExtra() {
+//		return horaExtra;
+//	}
+//	public void setHoraExtra(String horaExtra) {
+//		this.horaExtra = horaExtra;
+//	}
+//	
+//
+//}
