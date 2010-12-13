c0ff8ef4be8ed51d63cd8e9db09fe8b4a4a2ea75
Tássio Virgínio <tassiovirginio@gmail.com>
2010-12-12 21:34:29 -0300

    Colocando as classes em java para scala.

---
+++ src/main/java/br/tvsolutions/ponto/util/RelatorioXLS.scala
@@ -0,0 +1,17 @@
+package br.tvsolutions.ponto.util
+
+import java.util.{ArrayList,Date,List}
+
+
+class RelatorioXLS {
+	
+	var nome:String = _
+	var periodo:String = _
+	var jornada:Date = _
+	var horaEntrada:String = _
+	var horaSaida:String = _
+	var horaExtra:String = _
+	var listaPontos:List[PontoXLS] = new ArrayList[PontoXLS]();
+	var nomeXLS:String = _
+
+}
