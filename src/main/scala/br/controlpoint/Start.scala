02b49d0a7bc6b76124ab9de29f52ed9a7bbd713f
Tassio Virginio <tassiovirginio@gmail.com>
2016-03-09 10:26:13 -0300

    retirando bibliotecas nao utilizadas

---
+++ src/main/scala/br/controlpoint/Start.scala
@@ -0,0 +1,37 @@
+package br.controlpoint
+
+import org.apache.wicket.util.time.Duration
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server.bio.SocketConnector
+import org.eclipse.jetty.webapp.WebAppContext
+
+object Start {
+
+  def main(args: Array[String]) {
+    val timeout: Int = Duration.ONE_HOUR.getMilliseconds.toInt
+    var webPort: String = System.getenv("PORT")
+    if (webPort == null || webPort.isEmpty) {
+      webPort = "9999"
+    }
+    val server: Server = new Server
+    val connector: SocketConnector = new SocketConnector
+    connector.setMaxIdleTime(timeout)
+    connector.setSoLingerTime(-1)
+    connector.setPort(Integer.valueOf(webPort))
+    server.addConnector(connector)
+    val bb: WebAppContext = new WebAppContext
+    bb.setServer(server)
+    bb.setContextPath("/")
+    bb.setWar("src/main/webapp")
+    server.setHandler(bb)
+    try {
+      server.start
+      server.join
+    }
+    catch {
+      case e: Exception => {
+        e.printStackTrace
+      }
+    }
+  }
+}
